5b7fc89b584c6cb943b7fae611f6217b87d3305e
Chris <chris@Chriss-MacBook-Air.local>
2025-09-22 10:58:01 -0600

    setup cards, and card effect styling

---
+++ src/app/features/prayer-cards/prayer-cards.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-prayer-cards',
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
+  templateUrl: './prayer-cards.html',
+  styleUrl: './prayer-cards.css'
+})
+export class PrayerCards implements OnInit {
+ prayers: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadPrayers();
+  }
+
+  loadPrayers() {
+    this.http.get<{ data: any[] }>('http://localhost:4000/api/prayers')
+      .subscribe({
+        next: (res) => this.prayers = res.data,
+        error: (err) => console.error('❌ Error loading prayers:', err)
+      });
+  }
+  
+}
