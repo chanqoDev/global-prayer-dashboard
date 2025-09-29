274fae40d118bbc2527cc15d5be2067f0bb405af
Christian Elena <christianelena92@gmail.com>
2025-09-28 23:31:47 -0600

    update FormControl schema to contain region and update the form, also added additonal safety requirements for the form urgency field.

--- models/Prayer.js
+++ models/Prayer.js
@@ -3,10 +3,23 @@ import mongoose from "mongoose";
 const prayerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
+  region: {
+    type: String,
+    required: true, 
+    default: 'USA', 
+  }, 
   request: { type: String, required: true },
   dateRaw: { type: String },        // ISO string
   dateFormatted: { type: String },  // es-MX display string
-  urgency: { type: String },
+  urgency: { type: String, 
+    enum: ["high", "medium", "low"], 
+    default: "low",
+    required: true 
+  },
+  region: { 
+  type: String,
+  required: true
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
