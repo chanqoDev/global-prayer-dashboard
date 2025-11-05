87edecdeb0473bb95b0cdcf1e033bbca68b54f23
Chris <chris@Chriss-MacBook-Air.local>
2025-11-05 17:08:06 -0600

    create components for Home, About, 404 page not found, setup navbar routerLinks and paths, import image assets

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, OnInit } from '@angular/core';
 import { Grid } from 'gridjs';
 
 
@@ -9,7 +9,6 @@ import { Grid } from 'gridjs';
   templateUrl: '../../features/dashboard/dashboard.html',
 })
 export class DashboardComponent implements AfterViewInit {
-
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
@@ -36,3 +35,39 @@ export class DashboardComponent implements AfterViewInit {
       .catch(err => console.error('❌ Error loading prayers:', err));
   }
 }
+
+// export class DashboardComponent implements OnInit {
+
+//   constructor(private el: ElementRef) {}
+
+//   ngOnInit() {
+//     this.loadPrayers();
+//   }
+
+//   loadPrayers() {
+//     const container = this.el.nativeElement.querySelector('#prayerGrid');
+//     container.innerHTML = ""; // Clear old table before re-rendering
+
+//     fetch("https://global-prayer-dashboard.onrender.com/api/prayers")
+//       .then(res => res.json())
+//       .then(json => {
+//         const prayers = json.data;
+
+//         new Grid({
+//           columns: ['Name', 'Prayer Request', 'Date', 'Urgency'],
+//           data: prayers.map((p: { name: any; request: any; createdAt: string | number | Date; urgency: any; }) => [
+//             p.name,
+//             p.request,
+//             new Date(p.createdAt).toLocaleString(),
+//             p.urgency
+//           ]),
+//           search: true,
+//           sort: true,
+//           fixedHeader: true,
+//           height: '400px',
+//           pagination: { limit: 4 },
+//         }).render(container);
+//       })
+//       .catch(err => console.error('❌ Error loading prayers:', err));
+//   }
+// }
